574ae5a71703e089536837306270f604c934b1a8
Benjamin Pugh <ben.pugh@indicatura.com>
2022-11-02 15:37:04 +0000

    added beginning of login page

---
+++ src/LoginPage.jsx
@@ -0,0 +1,7 @@
+﻿class TopBanner extends React.Component {
+    render() {
+        return <h1 class="top-banner header-app-text">Nutri App</h1>
+    }
+};
+
+ReactDOM.render(<TopBanner />, document.getElementById('root'));
