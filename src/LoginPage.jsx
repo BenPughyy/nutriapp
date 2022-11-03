a801532812dc41c7c41ad20120d89cc8dea2e71b
Benjamin Pugh <ben.pugh@indicatura.com>
2022-11-03 10:32:35 +0000

    add login page

---
+++ src/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TopBanner extends React.Component {
+render() {
+    return <div>Nutri App</div>;
+}
+}
+
+ReactDOM.render(<TopBanner />, document.getElementByID('app'));
