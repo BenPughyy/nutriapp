08060abf0862014c1942c0b3401f308bc88892e4
Benjamin Pugh <ben.pugh@indicatura.com>
2022-11-07 08:59:38 +0000

    font family and weight added to landing page

--- src/components/styled.tsx
+++ src/components/styled.tsx
@@ -3,7 +3,7 @@ import { Header } from 'semantic-ui-react';
 
 export const HeaderContainer = styled.div`
   background-color: #cbcbcb;
-  height: 6vh;
+  height: 8vh;
   display: flex;
   justify-content: center;
   align-content: center;
@@ -12,4 +12,6 @@ export const HeaderContainer = styled.div`
 export const StyledHeader = styled(Header)`
   align-self: center;
   margin: 0;
+  font-weight: 400;
+  font-family: Happy Monkey;
 `;
