070f47950baeb6222880e33dc747028b6d3d8a48
Benjamin Pugh <ben.pugh@indicatura.com>
2023-01-04 16:21:50 +0000

    build fixed

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,10 +1,11 @@
 import { FC, useEffect } from 'react';
 import { Rating } from 'semantic-ui-react';
 import { Card, Image } from 'semantic-ui-react';
+
+//Hooks
 import useRecipes from '../../hooks/recipes/useRecipes';
 
-const RecipeCard: FC = () => {
-
+const RecipeCard: FC = (title: string, description: string) => {
 return (
     <Card>
         <Image
@@ -13,11 +14,11 @@ return (
         ui={false}
         />
         <Card.Content>
-            <Card.Header>{useRecipes.recipes.title}</Card.Header>
+            <Card.Header>{title}</Card.Header>
                 <Card.Meta>
                     <span className="date">Recipe by logged in user</span>
                 </Card.Meta>
-            <Card.Description>{useRecipes.recipes.description}</Card.Description>
+            <Card.Description>{description}</Card.Description>
         </Card.Content>
     </Card>
 );
--- src/pages/Recipes/RecipesPage.tsx
+++ src/pages/Recipes/RecipesPage.tsx
@@ -1,7 +1,12 @@
 import { FC, useEffect } from 'react';
 import { Card, Grid, Image } from 'semantic-ui-react';
+
+//Hooks
 import useRecipes from '../../hooks/recipes/useRecipes';
+
+//Components
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
+
 
 const RecipesPage: FC = () => {
   const { recipes, getRecipes } = useRecipes();
@@ -21,7 +26,7 @@ const RecipesPage: FC = () => {
           recipes.map((recipe) => {
             return (
               <Grid.Column>
-                <RecipeCard/>
+                <RecipeCard title={recipe.title} description={recipe.description}/>
               </Grid.Column>
             );
           })
